8ccaa1749371cde4fb3689708dcdff0e2ce83c22
LadakaTayaara <82630115+LadakaTayaara@users.noreply.github.com>
2025-09-03 16:10:24 +0530

    Update a4c.c
    assignment complete

--- a4c.c
+++ a4c.c
@@ -5,13 +5,25 @@
 char stack[100];
 char stack1[100][100];
 char postfix[100];
+char prefix[100];
 int top = -1;
 int top1 = -1;
 int n = 100;
 char popel;
 char popstr[100];
+char result[100];
 
+void reverseString(char* str) {
+    int length = strlen(str);
+    int i, j;
+    char temp;
 
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
 
 int isFull(int current_top, int max_size) {
     return current_top == max_size - 1;
@@ -74,6 +86,14 @@ int icp(char c) {
     return 0;
 }
 
+int pre_icp(char c) {
+    if (c == '(') return 5;
+    if (c == '^') return 3;
+    if (c == '*' || c == '/' || c == '%') return 2;
+    if (c == '+' || c == '-') return 1;
+    return 0;
+}
+
 int isp(char c) {
     if (c == '(') return 0;
     if (c == '^') return 3;
@@ -82,7 +102,15 @@ int isp(char c) {
     return 0;
 }
 
-void pstfix(char input[]) {
+int pre_isp(char c) {
+    if (c == '(') return 0;
+    if (c == '^') return 4;
+    if (c == '*' || c == '/' || c == '%') return 2;
+    if (c == '+' || c == '-') return 1;
+    return 0;
+}
+
+void infix_to_postfix(char input[]) {
     int i = 0;
     int j = 0;
     while (input[i] != '\0') {
@@ -112,7 +140,50 @@ void pstfix(char input[]) {
     postfix[j] = '\0';
 }
 
-void infx(char input[]) {
+void infix_to_prefix(char input[]) {
+    int i = 0;
+    int j = 0;
+    char temp_prefix[100];
+    reverseString(input);
+    while (input[i] != '\0') {
+        if (input[i] == '(') {
+            input[i] = ')';
+        } else if (input[i] == ')') {
+            input[i] = '(';
+        }
+        i++;
+    }
+    i = 0;
+    while (input[i] != '\0') {
+        char current_char = input[i];
+        if (isalnum(current_char)) {
+            temp_prefix[j++] = current_char;
+        } else if (current_char == '(') {
+            push(current_char);
+        } else if (current_char == ')') {
+            while (!isEmpty(top) && stack[top] != '(') {
+                temp_prefix[j++] = pop();
+            }
+            if (!isEmpty(top) && stack[top] == '(') {
+                pop();
+            }
+        } else {
+            while (!isEmpty(top) && pre_isp(stack[top]) > pre_icp(current_char)) {
+                temp_prefix[j++] = pop();
+            }
+            push(current_char);
+        }
+        i++;
+    }
+    while (!isEmpty(top)) {
+        temp_prefix[j++] = pop();
+    }
+    temp_prefix[j] = '\0';
+    reverseString(temp_prefix);
+    strcpy(prefix, temp_prefix);
+}
+
+void postfix_to_infix(char input[]) {
     int i = 0;
     char op1[100];
     char op2[100];
@@ -138,30 +209,134 @@ void infx(char input[]) {
     }
 }
 
+void postfix_to_prefix(char input[]) {
+    int len = strlen(input);
+    char op1[100];
+    char op2[100];
+    char temp[100];
+
+    for(int i = 0; i < len; i++) {
+        char tkn = input[i];
+        if (isalnum(tkn)) {
+            char temp_str[2] = {tkn, '\0'};
+            push1(temp_str);
+        } else {
+            pop1(op2);
+            pop1(op1);
+            strcpy(temp, "");
+            char tkn_to_str[2] = {tkn, '\0'};
+            strcat(temp, tkn_to_str);
+            strcat(temp, op1);
+            strcat(temp, op2);
+            push1(temp);
+        }
+    }
+}
+
+void prefix_to_infix(char input[]) {
+    int len = strlen(input);
+    char op1[100];
+    char op2[100];
+    char temp[100];
+
+    for(int i = len - 1; i >= 0; i--) {
+        char tkn = input[i];
+        if (isalnum(tkn)) {
+            char temp_str[2] = {tkn, '\0'};
+            push1(temp_str);
+        } else {
+            pop1(op1);
+            pop1(op2);
+            strcpy(temp, "(");
+            strcat(temp, op1);
+            char tkn_to_str[2] = {tkn, '\0'};
+            strcat(temp, tkn_to_str);
+            strcat(temp, op2);
+            strcat(temp, ")");
+            push1(temp);
+        }
+    }
+}
+
+void prefix_to_postfix(char input[]) {
+    int len = strlen(input);
+    char op1[100];
+    char op2[100];
+    char temp[100];
+
+    for(int i = len - 1; i >= 0; i--) {
+        char tkn = input[i];
+        if (isalnum(tkn)) {
+            char temp_str[2] = {tkn, '\0'};
+            push1(temp_str);
+        } else {
+            pop1(op1);
+            pop1(op2);
+            strcat(temp, op1);
+            strcat(temp, op2);
+            char tkn_to_str[2] = {tkn, '\0'};
+            strcat(temp, tkn_to_str);
+            push1(temp);
+        }
+    }
+}
+
 int main() {
     char infix[100];
     char input_postfix[100];
-    char result_infix[100];
+    char input_prefix[100];
+    char output[100];
     int ch = 0;
     char elem;
     while (ch != -1) {
-        printf("\nFollowing are the stack operations that can be performed via this program on \n1.Convert to infix to postfix\n2.Convert Postfix to Infix\n3.Exit\nEnter your choice\n");
+        printf("\nFollowing are the stack operations that can be performed via this program on \n1.Infix to postfix\n2.Postfix to Infix\n3.Postfix to prefix\n4.Prefix to Infix\n5.Prefix to Postfix\n6.Infix to Prefix\n7.Exit\nEnter your choice\n");
         scanf("%d", &ch);
         switch (ch) {
             case 1:
                 printf("Enter an infix expression: ");
                 scanf("%s", infix);
-                pstfix(infix);
+                infix_to_postfix(infix);
                 printf("The postfix expression is: %s\n", postfix);
                 break;
             case 2:
                 printf("Enter a postfix expression: ");
                 scanf("%s", input_postfix);
-                infx(input_postfix);
-                pop1(result_infix);
-                printf("The infix expression is %s\n", result_infix);
+                postfix_to_infix(input_postfix);
+                pop1(output);
+                printf("The infix expression is %s\n", output);
+                output[0]='\0';
                 break;
             case 3:
+                printf("Enter a postfix expression: ");
+                scanf("%s", input_postfix);
+                postfix_to_prefix(input_postfix);
+                pop1(output);
+                printf("The prefix expression is %s\n", output);
+                output[0]='\0';
+                break;
+            case 4:
+                printf("Enter a prefix expression: ");
+                scanf("%s", input_prefix);
+                prefix_to_infix(input_prefix);
+                pop1(output);
+                printf("The infix expression is %s\n", output);
+                output[0]='\0';
+                break;
+            case 5:
+                printf("Enter a prefix expression: ");
+                scanf("%s", input_prefix);
+                prefix_to_postfix(input_prefix);
+                pop1(output);
+                printf("The postfix expression is %s\n", output);
+                output[0]='\0';
+                break;
+            case 6:
+                printf("Enter an infix expression: ");
+                scanf("%s", infix);
+                infix_to_prefix(infix);
+                printf("The prefix expression is: %s\n", prefix);
+                break;
+            case 7:
                 ch = -1;
                 break;
             default:
