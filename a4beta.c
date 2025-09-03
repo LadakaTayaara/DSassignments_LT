210b06e83c7a10b234564304ba58f136f30e10f9
LadakaTayaara <82630115+LadakaTayaara@users.noreply.github.com>
2025-09-03 14:50:09 +0530

    Update a4beta.c

--- a4beta.c
+++ a4beta.c
@@ -3,29 +3,26 @@
 #include <ctype.h>
 
 char stack[100];
+char stack1[100][100];
 char postfix[100];
 int top = -1;
+int top1 = -1;
 int n = 100;
 char popel;
+char popstr[100];
 
-int isFull(char stack[100], int current_top, int max_size) {
-    if (current_top == max_size - 1) {
-        return 1;
-    } else {
-        return 0;
-    }
+
+
+int isFull(int current_top, int max_size) {
+    return current_top == max_size - 1;
 }
 
-int isEmpty(char stack[100], int current_top, int max_size) {
-    if (current_top == -1) {
-        return 1;
-    } else {
-        return 0;
-    }
+int isEmpty(int current_top) {
+    return current_top == -1;
 }
 
 void push(char elem) {
-    if (isFull(stack, top, n)) {
+    if (isFull(top, n)) {
         printf("\nStack is full\n");
     } else {
         top++;
@@ -34,7 +31,7 @@ void push(char elem) {
 }
 
 char pop() {
-    if (isEmpty(stack, top, n)) {
+    if (isEmpty(top)) {
         return '\0';
     } else {
         popel = stack[top];
@@ -43,8 +40,30 @@ char pop() {
     }
 }
 
-void display(char stack[100]){
-    printf("\n%c <------ TOP\n",stack[top]);
+void push1(char elem[]) {
+    if (isFull(top1, n)) {
+        printf("\nStack is full\n");
+    } else {
+        top1++;
+        strcpy(stack1[top1], elem);
+    }
+}
+
+void pop1(char result[]) {
+    if (isEmpty(top1)) {
+        result[0] = '\0';
+    } else {
+        strcpy(result, stack1[top1]);
+        top1--;
+    }
+}
+
+void display_char_stack() {
+    if (isEmpty(top)) {
+        printf("\nStack is empty\n");
+    } else {
+        printf("\n%c <------ TOP\n", stack[top]);
+    }
 }
 
 int icp(char c) {
@@ -73,21 +92,21 @@ void pstfix(char input[]) {
         } else if (current_char == '(') {
             push(current_char);
         } else if (current_char == ')') {
-            while (!isEmpty(stack, top, n) && stack[top] != '(') {
+            while (!isEmpty(top) && stack[top] != '(') {
                 postfix[j++] = pop();
             }
-            if (!isEmpty(stack, top, n) && stack[top] == '(') {
+            if (!isEmpty(top) && stack[top] == '(') {
                 pop();
             }
         } else {
-            while (!isEmpty(stack, top, n) && isp(stack[top]) >= icp(current_char)) {
+            while (!isEmpty(top) && isp(stack[top]) >= icp(current_char)) {
                 postfix[j++] = pop();
             }
             push(current_char);
         }
         i++;
     }
-    while (!isEmpty(stack, top, n)) {
+    while (!isEmpty(top)) {
         postfix[j++] = pop();
     }
     postfix[j] = '\0';
@@ -95,77 +114,64 @@ void pstfix(char input[]) {
 
 void infx(char input[]) {
     int i = 0;
-    int j = 0;
+    char op1[100];
+    char op2[100];
+    char temp[100];
+
     while (input[i] != '\0') {
-        char current_char = input[i];
-        if (isalnum(current_char)) {
-            postfix[j++] = current_char;
-        } else if (current_char == '(') {
-            push(current_char);
-        } else if (current_char == ')') {
-            while (!isEmpty(stack, top, n) && stack[top] != '(') {
-                postfix[j++] = pop();
-            }
-            if (!isEmpty(stack, top, n) && stack[top] == '(') {
-                pop();
-            }
+        char tkn = input[i];
+        if (isalnum(tkn)) {
+            char temp_str[2] = {tkn, '\0'};
+            push1(temp_str);
         } else {
-            while (!isEmpty(stack, top, n) && isp(stack[top]) >= icp(current_char)) {
-                postfix[j++] = pop();
-            }
-            push(current_char);
+            pop1(op2);
+            pop1(op1);
+            strcpy(temp, "(");
+            strcat(temp, op1);
+            char tkn_to_str[2] = {tkn, '\0'};
+            strcat(temp,tkn_to_str);
+            strcat(temp, op2);
+            strcat(temp, ")");
+            push1(temp);
         }
         i++;
     }
-    while (!isEmpty(stack, top, n)) {
-        postfix[j++] = pop();
-    }
-    postfix[j] = '\0';
 }
 
 int main() {
     char infix[100];
-    char postfix[100];
-    int ch=0;
+    char input_postfix[100];
+    char result_infix[100];
+    int ch = 0;
     char elem;
-     while(ch!=-1){
-        printf("\nFollowing are the stack operations that can be performed via this program on \n1.Display Top\n2.Push\n3.Pop\n4.Convert to infix to postfix\n5.Postfix to Infix\n6.Exit\nEnter your choice\n");
-        scanf("%d",&ch);
-        switch(ch){
+    while (ch != -1) {
+        printf("\nFollowing are the stack operations that can be performed via this program on \n1.Display Top\n2.Push\n3.Pop\n4.Exit\nEnter your choice\n");
+        scanf("%d", &ch);
+        switch (ch) {
             case 1:
-                display(stack);
+                display_char_stack();
                 break;
             case 2:
-                if(isFull(stack,top,n)){
+                if (isFull(top, n)) {
                     printf("Stack is full\n");
-                }
-                else{
+                } else {
                     printf("\nenter the element to push\n");
-                    scanf(" %c",&elem);
+                    scanf(" %c", &elem);
                     push(elem);
                 }
                 break;
             case 3:
-                if(isEmpty(stack,top,n)){
+                if (isEmpty(top)) {
                     printf("Stack is empty\n");
+                } else {
+                    pop();
                 }
-                else{
-                    pop(stack,top,n);
-                }
                 break;
-            case 4:printf("Enter an infix expression: ");
-                   scanf("%s", infix);
-                   pstfix(infix);
-                   printf("The postfix expression is: %s\n", postfix);
-                   break;
-            case 5:
-                printf("Enter an infix expression: ");
-                scanf("%s", infix);
+            case 4:
+                ch = -1;
                 break;
-            case 6:
-                ch=-1;
-                break;
-                default:printf("Invalid choice\n");
+            default:
+                printf("Invalid choice\n");
         }
     }
     return 0;
